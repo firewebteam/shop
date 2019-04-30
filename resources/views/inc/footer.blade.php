abaa6b25c2b8f1a625ad3f074ad8193892da109e
Gdzieniegdzie <zawistowski.h@interia.pl>
2019-04-30 22:50:36 +0500

    editing and removing

---
+++ resources/views/inc/footer.blade.php
@@ -0,0 +1,40 @@
+<footer class="pt-4 my-md-5 pt-md-5 border-top bg-dark" style="text-align: center; position: fixed; bottom: 0; width: 100%;">
+        <div class="row">
+          <div class="col-4">
+            <div class="row">
+                <div class="col-6">
+                    <img class="mb-2" src="/docs/4.3/assets/brand/bootstrap-solid.svg" alt="" width="24" height="24">
+                    <small class="d-block mb-3 text-muted">&copy; 2017-2019</small>
+                </div>
+                <div class="col-6">
+                    <h5>Jan Robi</h5>
+                    <ul class="list-unstyled text-small">
+                     <li class="text-muted">Robi Jan</li>
+                     <li class="text-muted">ul. Jana 21/37</li>
+                     <li class="text-muted">12-345</li>
+                    </ul>
+                </div>
+            </div>
+          </div>
+
+          <div class="col-4">
+            <h5>Media</h5>
+            <ul class="list-unstyled text-small">
+              <li><a class="text-muted" href="https://www.facebook.com/JanRobiJewellery/" target="_blank">Facebook</a></li>
+              <li><a class="text-muted" href="#">Resource name</a></li>
+              <li><a class="text-muted" href="#">Another resource</a></li>
+              <li><a class="text-muted" href="#">Final resource</a></li>
+            </ul>
+          </div>
+
+          <div class="col-4">
+            <h5>About</h5>
+            <ul class="list-unstyled text-small">
+              <li><a class="text-muted" href="#">Team</a></li>
+              <li><a class="text-muted" href="#">Locations</a></li>
+              <li><a class="text-muted" href="#">Privacy</a></li>
+              <li><a class="text-muted" href="#">Terms</a></li>
+            </ul>
+        </div>
+    </div>
+</footer>
