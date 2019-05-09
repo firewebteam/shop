ee343ead68d8cc3e5bba2dc4bdad1093819e0699
Gdzieniegdzie <zawistowski.h@interia.pl>
2019-05-09 17:09:44 +0500

    v1.1

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -4,9 +4,27 @@
 
 <main role="main" class="container">
 
-      <div class="center">
-        <h1>Jeżeli chodzi o żubry to opiekuje sie nimi moja matka</h1>
-        <p class="lead">Ona ma tam dziuple</p>
+   
+        
+          
+      <div class="form-group text-center " style = 'margin-top: 100px'>
+        <h1> Zajmujemy się wykonywaniem biżuterii całkowicie własnoręcznie! </h1>
+        <img src = "https://www.leron.cz/sk/img/handmade.png">
+      <br><br><br> <br><br><br>
+
+        <h1> W naszej pracowni możesz zobaczyć jak naprawdę wygląda praca </h1>
+        <img src = "http://hobbydom.pl/wp-content/uploads/2014/11/domidrewno_3.jpg"  style="width: 800px">
+        <br><br><br> <br><br><br>
+
+        <h1> Posiadamy żywicę najwyższej jakości </h1>
+        <img src = "https://www.elemenciki.pl/6975-large_default/zywica-do-bizuterii-odlewow-zywica-epoksydowa-bezbarwna-035kg.jpg">
+        <br><br><br> <br><br><br>
+
+        <h1>Drewno sprowadzamy z wielu krajów Europy i Azji </h1>
+        <img src = "http://ludwinscy.pl/wp-content/uploads/2011/08/tarcica-1.jpg"  style="width: 800px">
+
+
+
       </div>
     
 </main>
