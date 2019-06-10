e1f600d558de3c30d0173d6cad582f51e0416bf6
unknown <suder.jacek@gmail.com>
2019-06-10 17:41:43 +0200

    poprawki jezykowe po raz kolejny

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -5,17 +5,17 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                <div class="card-header">{{ __('Zweryfikuj adres E-Mail') }}</div>
 
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                            {{ __('Nowy link weryfikacyjny został wysłany na twój E-Mail') }}
                         </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }}, <a href="{{ route('verification.resend') }}">{{ __('click here to request another') }}</a>.
+                    {{ __('Zanim przejdziesz dalej, sprawdź czy na twoją skrzynkę nie dotarł mail weryfikacyjny') }}
+                    {{ __('Jeśli nie otrzymałeś wiadomości, ') }}, <a href="{{ route('verification.resend') }}">{{ __('kliknij tutaj aby wysłać ją ponownie') }}</a>.
                 </div>
             </div>
         </div>
