1423375af199d2103cff0ce4ca309d125c5bcc2c
unknown <suder.jacek@gmail.com>
2019-06-10 17:44:30 +0200

    poprawka zdjec na stronie tworcy po raz pierwszy

--- resources/views/pages/creators.blade.php
+++ resources/views/pages/creators.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
         <div class="col-sm-6" style="text-align: center">
             <h1>Hubert Zawistowski</h1>
-            <img  height="50px" height="100%" src="https://scontent.fwaw3-1.fna.fbcdn.net/v/t1.0-9/11082671_840999852626144_8755165040654753250_n.jpg?_nc_cat=100&_nc_ht=scontent.fwaw3-1.fna&oh=f38ed875e5eed88a4c37978258ef98ea&oe=5D5AE24E">
+            <img  height="400px" height="100%" src="https://scontent.fwaw3-1.fna.fbcdn.net/v/t1.0-9/11082671_840999852626144_8755165040654753250_n.jpg?_nc_cat=100&_nc_ht=scontent.fwaw3-1.fna&oh=f38ed875e5eed88a4c37978258ef98ea&oe=5D5AE24E">
             <h3>Wykształcenie</h3>
             <ul>
                 <li>3 Rok Inżynierii Bezpieczeństwa Pożarowego dla strażaków w służbie kandydackiej Wydziału Inżynierii Bezpieczeństwa Pożarowego Szkoły Głównej Służby Pożarniczej</li>
@@ -15,7 +15,7 @@
 
         <div class="col-sm-6" style="text-align: center">
             <h1>Jacek Suder</h1>
-            <img height="50px" height="100%" src="https://scontent.fwaw3-1.fna.fbcdn.net/v/t1.0-9/18519653_1245099112267667_5476342812528068762_n.jpg?_nc_cat=102&_nc_ht=scontent.fwaw3-1.fna&oh=225dcf1da1daf312062cd7d00a08c01d&oe=5D5D0C5E">
+            <img height="400px" height="100%" src="https://scontent.fwaw3-1.fna.fbcdn.net/v/t1.0-9/18519653_1245099112267667_5476342812528068762_n.jpg?_nc_cat=102&_nc_ht=scontent.fwaw3-1.fna&oh=225dcf1da1daf312062cd7d00a08c01d&oe=5D5D0C5E">
             <h3>Wykształcenie</h3>
             <ul>
                 <li>3 Rok Inżynierii Bezpieczeństwa Pożarowego dla strażaków w służbie kandydackiej Wydziału Inżynierii Bezpieczeństwa Pożarowego Szkoły Głównej Służby Pożarniczej</li>
